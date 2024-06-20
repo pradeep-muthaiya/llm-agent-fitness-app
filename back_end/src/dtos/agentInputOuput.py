cc504163a6eedbc13e2734a633621b2158948d8c
Pradeep Muthaiya <pradeep.muthaiya@berkeley.edu>
2024-06-20 12:20:28 -0700

    finished project base

--- back_end/src/dtos/agentInputOuput.py
+++ back_end/src/dtos/agentInputOuput.py
@@ -1,5 +1,8 @@
-from pydantic.v1 import BaseModel, Field
+#from pydantic import BaseModel, Field
 from typing import List
+from pydantic import BaseModel, Field
+from langchain_core.pydantic_v1 import BaseModel as BaseModelResponse
+from langchain_core.pydantic_v1 import Field as FieldResponse
 
 class AgentInput(BaseModel):
     query: str = Field(description="Textual query of a list of foods")
@@ -14,3 +17,12 @@ class AgentOutput(BaseModel):
     calories: int = Field(description="The total amount of calories consumed")
     output: str = Field(description="list of all the foods that were consumed")
     
+# class Response(BaseModel):
+#     answer: str = Field(description="The final answer to respond to the user")
+#     total_calories: str = Field(description="The total number of calories in all of the foods combined")
+#     total_protein: str = Field(description="The total amount of protein in all of the foods combined")
+
+class Response(BaseModelResponse):
+    foodList: List[str] = FieldResponse(description="a list of all the foods that the user inputted")
+    calorieList: List[int] = FieldResponse(description="A list of the number of calories in each food the user submitted")
+    proteinList: List[int] = FieldResponse(description="A list of the protein that was in each food the user submitted")
--- back_end/src/main.py
+++ back_end/src/main.py
@@ -1,11 +1,27 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 import sys
+
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost:3000",
+    "http://localhost:8080",
+]
 
 sys.path.insert(0, "")
 from routers.router import router as router
 
 app = FastAPI(title="AI Fitness Application")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(router, prefix='/aifitnessapp')
 
